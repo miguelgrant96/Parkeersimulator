4ffcb39a652c4d9fb2827082ee5fa5f5ea4a19c6
kwiznix <m.f.scholten@st.hanze.nl>
2017-01-23 21:33:41 +0100

    parkeersimulatorsception verwijderd

--- src/Parkeersimulator/SimulatorView.java
+++ src/Parkeersimulator/SimulatorView.java
@@ -2,7 +2,7 @@ package Parkeersimulator;
 
 import javax.swing.*;
 import java.awt.*;
-
+//
 public class SimulatorView extends JFrame {
     private CarParkView carParkView;
     private int numberOfFloors;
