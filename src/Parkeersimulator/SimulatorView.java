348f1fffe313707df14b69070cd7bf8ee59093a3
kwiznix <m.f.scholten@st.hanze.nl>
2017-01-23 20:27:12 +0100

    testmessage

--- src/Parkeersimulator/SimulatorView.java
+++ src/Parkeersimulator/SimulatorView.java
@@ -2,7 +2,7 @@ package Parkeersimulator;
 
 import javax.swing.*;
 import java.awt.*;
-//test
+//test1
 public class SimulatorView extends JFrame {
     private CarParkView carParkView;
     private int numberOfFloors;
