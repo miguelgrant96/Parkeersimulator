742f8801da23fb7636fdd2ac44fbcb1214a4b1d8
Jop Bakker <joppie.b@live.nl>
2017-01-24 20:44:41 +0100

    Daily commit -- 24/1/2017
    Buttons werken weer + MVC

---
+++ src/Models/BetaalAutomaat.java
@@ -0,0 +1,20 @@
+package Models;
+
+/**
+ * Created by Jop on 24-1-2017.
+ */
+public class BetaalAutomaat {
+
+    private double kostenPM = 0.05;
+    private double kostenTotaal;
+    private AdHocCar car;
+
+    public BetaalAutomaat(){
+        kostenTotaal = getTimeParked() * kostenPM;
+    }
+
+    private int getTimeParked(){
+        return car.parkingTime();
+    }
+
+}
